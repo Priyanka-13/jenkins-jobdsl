e7c73b04da9c04568b828d271386d21ed5a20b29
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-04-26 11:35:17 +0530

    upadated codeDeploy dsl

--- javaProject/gradle/dslTemplates/codeDeploy.groovy
+++ javaProject/gradle/dslTemplates/codeDeploy.groovy
@@ -1,16 +1,44 @@
-
- job("CodeDeploy") {
-  description("Code deploy Job ")
-  logRotator {
-        daysToKeep(60)
-        numToKeep(20)
-        artifactDaysToKeep(1)
+job("CodeDeploy") {
+  description('Code deploy for DirectLogin')
+  parameters {
+        activeChoiceParam('REPOSITORY') {
+             
+            groovyScript {
+            fallbackScript('return["Script Error"]')
+            script('groovy script')
+            }
+       choiceType('SINGLE_SELECT')
+        }
+    
+   activeChoiceReactiveParam('BRANCH') {
+            
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('choice')
+               
+            }
+            referencedParameter('REPOSITORY')
+            
+        }
+    
+    activeChoiceReactiveParam('ARTIFACT') {
+            
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["choice"]')
+               
+            }
+            referencedParameter('REPOSITORY,BRANCH')
+            
+        }
+    
+    
+       choiceParam('Environment', ['Dev', 'QA', 'Production'])
     }
+  
   steps {
-	steps {
+        steps {
         shell('sudo ansible-playbook -i /etc/ansible/roles/code/hosts /etc/ansible/roles/code/site.yml')
-    }
-
+      }
     }
 }
-
