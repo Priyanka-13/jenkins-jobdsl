89d3bf1986a9315942af7a7987b2d05a41cbebfb
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-04-26 10:55:27 +0530

    added ci-creator job

---
+++ javaProject/gradle/dslTemplates/CI-creator.groovy
@@ -0,0 +1,35 @@
+job("ci-creator") {
+  parameters {
+        stringParam('ProjectName', '', 'Name of the Project')
+        stringParam('AppURL')
+        stringParam('GitBranch')
+  
+    choiceParam('GradleTask', ['jar', 'war', ])
+    }
+         
+   
+  scm {
+     git {
+      remote {
+        url("https://github.com/OpsTree/jenkins-jobdsl.git")
+      }
+       branch("*/master")
+     }
+   }
+        
+    steps {
+        groovyScriptFile('javaProject/gradle/dslGenerator.groovy') {
+            groovyInstallation('Default')
+            
+        }
+    }
+
+ steps {
+      dsl  {
+            external('javaProject/gradle/DirectLogin/*.groovy')
+            removeAction('IGNORE')
+            
+            
+        }
+    }   
+}
