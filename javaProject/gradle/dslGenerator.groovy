6348d6a82a135b2131b085458327deab8e05169c
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-04-26 13:20:13 +0530

    updated dsl generator to pick job only starts with 'Job'  keyword

--- javaProject/gradle/dslGenerator.groovy
+++ javaProject/gradle/dslGenerator.groovy
@@ -12,22 +12,22 @@ println "$projectname"
 
 new File("${WORKSPACE}/javaProject/gradle/$projectname").mkdirs()  
 
-new File("${WORKSPACE}/javaProject/gradle/dslTemplates").eachFile() { files2->  
-    files = files2.getName()  
-	println files
-	
-	def file = new File("${WORKSPACE}/javaProject/gradle/dslTemplates/${files}") 
-	def binding = ['projectname' : projectname , 'appurl' : appurl, 'gitbranch' : gitbranch, 'gradletask': gradletask ]
-				  
-	def engine = new SimpleTemplateEngine() 
-	def template = engine.createTemplate(file) 
-	def writable = template.make(binding) 
 
-//println writable
+new File("/var/lib/jenkins/workspace/seedJob/javaProject/gradle/dslTemplates").eachFile() { file->
+      files = file.getName()
+     
+    if(files.matches("Job(.*)")) {
+        //println files
+      
+        def file = new File("${WORKSPACE}/javaProject/gradle/dslTemplates/${files}")
+        def binding = ['projectname' : projectname , 'appurl' : appurl, 'gitbranch' : gitbranch, 'gradletask': gradletask ]
 
+        def engine = new SimpleTemplateEngine()
+        def template = engine.createTemplate(file)
+        def writable = template.make(binding)
  
 	new File("${WORKSPACE}/javaProject/gradle/${projectname}","${files}").withWriter('utf-8') { 
 	         writer -> writer.write (writable) 
   }
-
+ }
 }
