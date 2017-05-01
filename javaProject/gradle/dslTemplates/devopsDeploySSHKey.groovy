ef6ed1ad922e1756743c0ec5db9241a94472248f
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-05-01 14:02:17 +0530

    added dsl for ssh key deployment

---
+++ javaProject/gradle/dslTemplates/devopsDeploySSHKey.groovy
@@ -0,0 +1,36 @@
+job("DevopsDeploySSHKey") {
+  description('Deploy key on Bitbucket into specific requirement.')
+  
+   logRotator {
+        daysToKeep(60)
+        numToKeep(20)
+        artifactDaysToKeep(1)
+    }
+  
+  parameters {
+
+            activeChoiceReactiveParam('RepositoryName') {
+
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                fallbackScript('return["Script Error"]')
+                script('evaluate(new File("evaluate(new File("/var/lib/jenkins/scripts/getRepoList.groovy"))"))')
+
+            }
+
+        }
+    		fileParam {
+					name('SSHKeyFile')
+					description('Please include your ssh file which we be deployed to the Bitbucket Repo.')
+		}
+
+     }
+ 
+  steps {
+        shell(
+          'mv SSHKeyFile id_rsa.pub \n' +
+		  'bash ${JENKINS_HOME}/scripts/deploySSHKeyIntoBitbucket.sh ${RepositoryName} ${SSHKeyFile} \n'
+       )
+
+    }
+}
