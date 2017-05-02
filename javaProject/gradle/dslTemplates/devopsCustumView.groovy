b0fad9b5935c03c58dd86b46f22e9c6c1686f84c
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-05-02 18:47:55 +0530

    updated devopsCustumView.groovy for devopsciDestroyer.groovy

--- javaProject/gradle/dslTemplates/devopsCustumView.groovy
+++ javaProject/gradle/dslTemplates/devopsCustumView.groovy
@@ -1,6 +1,6 @@
 listView("Devops-Jobs") {
             jobs {
-                names("DevopsDeploySSHKey", "Devops-CI-creator")
+                names("DevopsDeploySSHKey", "Devops-CI-creator", "Devops-CI-destroyer")
             }
             columns {
                 status()
